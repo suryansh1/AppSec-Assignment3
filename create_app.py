e6e1a98254fbc81f3980766c1866cfe467d73792
suryansh1 <suryansh.1234@gmail.com>
2019-12-10 17:05:48 -0500

    Adding tests

--- create_app.py
+++ create_app.py
@@ -7,7 +7,9 @@ def app_creator():
 	app = Flask(__name__)
 
 	app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'app.db')
+	
 	app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+	
 	app.config.update(dict(
 	    SECRET_KEY="powerful secretkey",
 	    WTF_CSRF_SECRET_KEY="a csrf secret key"
